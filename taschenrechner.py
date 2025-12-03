00710818541639fc5889aaee1f2c06d61b21d44a
Stefan Schwarz <stefan.schwarz@cstx-mi.de>
2025-12-03 11:26:39 +0100

    Added multiplication & enum for operations

--- taschenrechner.py
+++ taschenrechner.py
@@ -1,5 +1,13 @@
 import tkinter as tk
 from tkinter import font
+from enum import Enum
+
+class SelectOperation(Enum):
+    NONE = 0
+    ADD = 1
+    SUB = 2
+    MULT = 3
+    DIV = 4
 
 root = tk.Tk()
 root.geometry("500x500")
@@ -21,15 +29,15 @@ Second_Number = 0
 
 Result = 0
 
-DoSUB = False
+selectedOperation = SelectOperation.NONE
 
 #Zahlen nach Eingabe merken
 def add_numbers():
     global first_Number
     first_Number = int(eingabe.get())
     eingabe.delete(0, tk.END)
-    global DoSUB
-    DoSUB = False
+    global selectedOperation
+    selectedOperation = SelectOperation.ADD
 
 #Zahlen Addieren
 def addition():
@@ -46,8 +54,8 @@ def SUB_Numbers():
     global first_Number
     first_Number = int(eingabe.get())
     eingabe.delete(0, tk.END)
-    global DoSUB
-    DoSUB = True
+    global selectedOperation
+    selectedOperation = SelectOperation.SUB
 
 
 #Zahlen Subtrahieren
@@ -58,6 +66,22 @@ def Subtraktion():
     global ResultSUB
     ResultSUB = first_Number - Second_Number
     eingabe.insert(tk.END, ResultSUB)
+
+# Button 'Multiplikation' gedrückt
+def MULT_Numbers():
+    global first_Number
+    first_Number = int(eingabe.get())
+    eingabe.delete(0, tk.END)
+    global selectedOperation
+    selectedOperation = SelectOperation.MULT
+
+# Zahlen multiplizieren
+def Multiplikation():
+    global Second_Number
+    Second_Number = int(eingabe.get())
+    eingabe.delete(0, tk.END)
+    result = first_Number * Second_Number
+    eingabe.insert(tk.END, result)
 
 
 #Zahlentasten
@@ -76,7 +100,7 @@ button0 = tk.Button(root, text="0", font=font_general,command=lambda: addChar ('
 button_plus = tk.Button(root, text="+", font=font_general, command=lambda: add_numbers ())
 button_minus = tk.Button(root, text="-", font=font_general,command=lambda: SUB_Numbers ())
 button_div = tk.Button(root, text="/", font=font_general,command=lambda: addChar ('/'))
-button_mult = tk.Button(root, text="*", font=font_general,command=lambda: addChar ('*'))
+button_mult = tk.Button(root, text="*", font=font_general,command=lambda: MULT_Numbers())
 
 #Ergebnis/Löschen
 button_clear = tk.Button(root, text="clear", font=font_general, command=lambda: eingabe.delete(0, tk.END))
@@ -84,11 +108,20 @@ button_result = tk.Button(root, text="=", font=font_general,command=lambda: Tota
 
 #Unterschied Addition/Subtraktion
 def Total():   
-    global DoSUB
-    if DoSUB:
-       Subtraktion()
-    else:
-        addition()
+    global selectedOperation
+    match selectedOperation:
+        case SelectOperation.NONE:
+            return
+        case SelectOperation.ADD:
+            addition()
+        case SelectOperation.SUB:
+            Subtraktion()
+        case SelectOperation.MULT:
+            Multiplikation()
+        case SelectOperation.DIV:
+            return
+            #Division
+    selectedOperation = SelectOperation.NONE
 
 #Aufteilung
 button7.grid(row=1,column=0)
